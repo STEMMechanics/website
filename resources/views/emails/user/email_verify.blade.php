e079100cc03f6b0de4f76b766a7cb9aa00a2ae6b
James Collins <james.collins@outlook.com.au>
2023-01-24 21:41:52 +1000

    updated email templates

--- resources/views/emails/user/email_verify.blade.php
+++ resources/views/emails/user/email_verify.blade.php
@@ -1,104 +1,135 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<html
+    lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+    xmlns="http://www.w3.org/1999/xhtml"
+    xmlns:v="urn:schemas-microsoft-com:vml"
+    xmlns:o="urn:schemas-microsoft-com:office:office"
+>
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>STEMMechanics - Verify Email Address</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>STEMMechanics - Forgot Password</title>
+        <link
+            rel="noopener"
+            target="_blank"
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&display=swap"
+            rel="stylesheet"
+        />
+        <!--[if gte mso 9]>
+            <xml>
+                <o:OfficeDocumentSettings>
+                    <o:AllowPNG />
+                    <o:PixelsPerInch>96</o:PixelsPerInch>
+                </o:OfficeDocumentSettings>
+            </xml>
+        <![endif]-->
         <style>
-            @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&display=swap');
-            * {
-                box-sizing: border-box;
-                margin: 0;
-                padding: 0;
-            }
-
-            body {
-                font-size: 1.1rem;
-                font-family: Nunito, Arial, Helvetica, sans-serif !important;
-                color: #000000;
-                padding: 2rem;
-                -webkit-font-smoothing: antialiased;
-                -moz-osx-font-smoothing: grayscale
-            }
-
-            div.main {
-                margin: 0 auto;
-                background-color: #ffffff;
-                overflow: hidden;
-            }
-
-            div.footer {
-                margin: 2rem auto;
-                max-width: 48rem;
-                font-size: 70%;
-                text-align: center;
-            }
-
-            a.brand {
-                display: block;
-                margin-bottom: 2rem;
-                padding: 0 2rem;
-            }
-
-            a.brand:hover {
-                text-decoration: none;
-            }
-
-            h2 {
-                margin-bottom: 2rem;
-            }
-
-            p {
-                margin-bottom: 1rem;
-            }
-
-            a.brand img {
-                width: 100%;
-                max-width: 100%;
-                object-fit: contain;
-            }
-
-            .code {
-                display: block;
-                font-size: 200%;
-                text-align: center;
-                margin-top: 2rem;
-                margin-bottom: 2rem;
-                letter-spacing: 0.5rem;
-            }
-
-            .feedback {
-                font-size: 90%;
-                text-align: center;
-            }
-
-            .border {
-                border-top: 1px solid #ddd;
-                margin-bottom: 2rem;
-            }
-
-            a, a:visited, a:hover {
-                color: #2563EB;
-            }
-
-            a:hover {
-                text-decoration: underline;
-            }
+            @import url("https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&display=swap");
         </style>
     </head>
     <body>
-        <div class="main">
-            <a href="https://www.stemmechanics.com.au/" class="brand">
-                <img alt="STEMMechanics Logo" src="{{ $message->embed(public_path('img').'/logo.png') }}">
-            </a>
-            <h2>Welcome {{ $user?->username }},</h2>
-            <p>We've heard you would like to try out our workshops and courses!</p>
-            <p>Before we can let you loose on our website, we need to make sure you are a real person and not a pesky robot or cat. Click this link <a href="https://www.stemmechanics.com.au/verify-email?code={{ $code }}">stemmechanics.com.au/verify-email</a> and if you are asked, use the confirm code:</p>
-            <strong class="code">{{ $code }}</strong>
-            <div class="border"></div>
-            <p class="feedback">Need help or got feedback? <a href="https://www.stemmechanics.com.au/contact">Contact us</a> or touch base at <a href="https://twitter.com/stemmechanics">@stemmechanics</a>.</p>
-        </div>
-        <div class="footer">Sent by STEMMechanics &middot; <a href="https://www.stemmechanics.com.au/">Visit our Website</a> &middot; <a href="https://twitter.com/stemmechanics">@stemmechanics</a><br>PO Box 36, Edmonton, QLD 4869, Australia</div>
+        <table
+            cellspacing="0"
+            cellpadding="0"
+            border="0"
+            role="presentation"
+            style="
+                width: 100%;
+                padding: 2rem;
+                font-size: 1.1rem;
+                color: #000000;
+                font-family: Nunito, Arial, Helvetica, sans-serif;
+                -webkit-font-smoothing: antialiased;
+                -moz-osx-font-smoothing: grayscale;
+            "
+        >
+            <tr>
+                <td>
+                    <a href="https://www.stemmechanics.com.au/">
+                        <img
+                            alt="STEMMechanics Logo"
+                            src="{{ $message->embed(public_path('img').'/logo.png') }}"
+                            width="400"
+                            height="62"
+                        />
+                    </a>
+                </td>
+            </tr>
+            <tr>
+                <td><h2>Welcome {{ $user?->username }},</h2></td>
+            </tr>
+            <tr>
+                <td>
+                    We've heard you would like to try out our workshops and courses!
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Before we can let you loose on our website, we need to make sure you are a real person and not a pesky robot or cat. Click this link <a href="https://www.stemmechanics.com.au/verify-email?code={{ $code }}">stemmechanics.com.au/verify-email</a> and if you are asked, use the confirm code:
+                </td>
+            </tr>
+            <tr>
+                <td
+                    align="center"
+                    style="
+                        font-size: 200%;
+                        text-align: center;
+                        padding-top: 2rem;
+                        padding-bottom: 2rem;
+                        letter-spacing: 0.5rem;
+                    "
+                >
+                    <strong>{{ $code }}</strong>
+                </td>
+            </tr>
+            <tr>
+                <td style="padding-bottom: 2rem">
+                    But if you didn't ask to reset your password, you can delete
+                    this email and your password will remain the same.
+                </td>
+            </tr>
+            <tr>
+                <td
+                    align="center"
+                    style="
+                        font-size: 90%;
+                        text-align: center;
+                        padding-top: 2rem;
+                        padding-bottom: 2rem;
+                        border-top: 1px solid #ddd;
+                    "
+                >
+                    Need help or got feedback?
+                    <a href="https://www.stemmechanics.com.au/contact"
+                        >Contact us</a
+                    >
+                    or touch base at
+                    <a href="https://twitter.com/stemmechanics"
+                        >@stemmechanics</a
+                    >.
+                </td>
+            </tr>
+            <tr>
+                <td
+                    align="center"
+                    style="
+                        font-size: 80%;
+                        text-align: center;
+                        padding-top: 1rem;
+                        padding-bottom: 2rem;
+                    "
+                >
+                    Sent by STEMMechanics &middot;
+                    <a href="https://www.stemmechanics.com.au/"
+                        >Visit our Website</a
+                    >
+                    &middot;
+                    <a href="https://twitter.com/stemmechanics"
+                        >@stemmechanics</a
+                    ><br />PO Box 36, Edmonton, QLD 4869, Australia
+                </td>
+            </tr>
+        </table>
     </body>
 </html>
--- resources/views/emails/user/forgot_username.blade.php
+++ resources/views/emails/user/forgot_username.blade.php
@@ -1,132 +1,131 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<html
+    lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+    xmlns="http://www.w3.org/1999/xhtml"
+    xmlns:v="urn:schemas-microsoft-com:vml"
+    xmlns:o="urn:schemas-microsoft-com:office:office"
+>
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>STEMMechanics - Forgot Username</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>STEMMechanics - Forgot Password</title>
+        <link
+            rel="noopener"
+            target="_blank"
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&display=swap"
+            rel="stylesheet"
+        />
+        <!--[if gte mso 9]>
+            <xml>
+                <o:OfficeDocumentSettings>
+                    <o:AllowPNG />
+                    <o:PixelsPerInch>96</o:PixelsPerInch>
+                </o:OfficeDocumentSettings>
+            </xml>
+        <![endif]-->
         <style>
-            @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&display=swap');
-            * {
-                box-sizing: border-box;
-                margin: 0;
-                padding: 0;
-            }
-
-            body {
-                font-size: 1.1rem;
-                font-family: Nunito, Arial, Helvetica, sans-serif !important;
-                color: #000000;
-                padding: 2rem;
-                -webkit-font-smoothing: antialiased;
-                -moz-osx-font-smoothing: grayscale
-            }
-
-            div.main {
-                margin: 0 auto;
-                background-color: #ffffff;
-                overflow: hidden;
-            }
-
-            div.footer {
-                margin: 2rem auto;
-                max-width: 48rem;
-                font-size: 70%;
-                text-align: center;
-            }
-
-            a.brand {
-                display: block;
-                margin-bottom: 2rem;
-                padding: 0 2rem;
-            }
-
-            a.brand:hover {
-                text-decoration: none;
-            }
-
-            h2 {
-                margin-bottom: 2rem;
-            }
-
-            p {
-                margin-bottom: 1rem;
-            }
-
-            a.brand img {
-                width: 100%;
-                max-width: 100%;
-                object-fit: contain;
-            }
-
-            .code {
-                display: block;
-                font-size: 200%;
-                text-align: center;
-                margin-top: 2rem;
-                margin-bottom: 2rem;
-                letter-spacing: 0.5rem;
-            }
-
-            .feedback {
-                font-size: 90%;
-                text-align: center;
-            }
-
-            .border {
-                border-top: 1px solid #ddd;
-                margin-bottom: 2rem;
-            }
-
-            a, a:visited, a:hover {
-                color: #2563EB;
-            }
-
-            a:hover {
-                text-decoration: underline;
-            }
-
-            div.list {
-                padding-left: 2rem;
-                padding-right: 2rem;
-                /* text-align: center; */
-            }
-
-            div.list ul {
-                list-style-position: inside;
-                display: inline-block;
-                text-align: left;
-                margin-bottom: 2rem;
-            }
+            @import url("https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&display=swap");
         </style>
     </head>
     <body>
-        <div class="main">
-            <a href="https://www.stemmechanics.com.au/" class="brand">
-                <img alt="STEMMechanics Logo" src="{{ $message->embed(public_path('img').'/logo.png') }}">
-            </a>
-            @if (count($usernames) > 2)
-            <h2>Yo {{ $usernames[0] }}, {{ $usernames[1] }}, or is it {{ $usernames[count($usernames)-1] }}?</h2>
-            @elseif (count($usernames) > 1)
-            <h2>Yo {{ $usernames[0] }} or is it {{ $usernames[1] }}?</h2>
-            @else
-            <h2>Yo {{ $usernames[0] }},</h2>
-            @endif
-            @if (count($usernames) == 1)
-            <p>Guess what, your username is {{ $usernames[0] }}.</p>
-            @else
-            <p>We have the following usernames registered to this email address:</p>
-            <div class="list">
-                <ul>
-                    @foreach($usernames as $username)
-                    <li>{{ $username }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @endif
-            <div class="border"></div>
-            <p class="feedback">Need help or got feedback? <a href="https://www.stemmechanics.com.au/contact">Contact us</a> or touch base at <a href="https://twitter.com/stemmechanics">@stemmechanics</a>.</p>
-        </div>
-        <div class="footer">Sent by STEMMechanics &middot; <a href="https://www.stemmechanics.com.au/">Visit our Website</a> &middot; <a href="https://twitter.com/stemmechanics">@stemmechanics</a><br>PO Box 36, Edmonton, QLD 4869, Australia</div>
+        <table
+            cellspacing="0"
+            cellpadding="0"
+            border="0"
+            role="presentation"
+            style="
+                width: 100%;
+                padding: 2rem;
+                font-size: 1.1rem;
+                color: #000000;
+                font-family: Nunito, Arial, Helvetica, sans-serif;
+                -webkit-font-smoothing: antialiased;
+                -moz-osx-font-smoothing: grayscale;
+            "
+        >
+            <tr>
+                <td>
+                    <a href="https://www.stemmechanics.com.au/">
+                        <img
+                            alt="STEMMechanics Logo"
+                            src="{{ $message->embed(public_path('img').'/logo.png') }}"
+                            width="400"
+                            height="62"
+                        />
+                    </a>
+                </td>
+            </tr>
+            <tr>
+                <td>            
+                    @if (count($usernames) > 2)
+                    <h2>Yo {{ $usernames[0] }}, {{ $usernames[1] }}, or is it {{ $usernames[count($usernames)-1] }}?</h2>
+                    @elseif (count($usernames) > 1)
+                    <h2>Yo {{ $usernames[0] }}, or is it {{ $usernames[1] }}?</h2>
+                    @else
+                    <h2>Yo {{ $usernames[0] }},</h2>
+                    @endif
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    @if (count($usernames) == 1)
+                    Guess what, your username is <strong>{{ $usernames[0] }}</strong>.
+                    @else
+                    We have the following usernames registered to this email address:
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <ul style="padding-top: 2rem; padding-bottom: 2rem;">
+                        @foreach($usernames as $username)
+                        <li>{{ $username }}</li>
+                        @endforeach
+                    </ul>
+                    @endif
+                </td>
+            </tr>
+            <tr>
+                <td
+                    align="center"
+                    style="
+                        font-size: 90%;
+                        text-align: center;
+                        padding-top: 2rem;
+                        padding-bottom: 2rem;
+                        border-top: 1px solid #ddd;
+                    "
+                >
+                    Need help or got feedback?
+                    <a href="https://www.stemmechanics.com.au/contact"
+                        >Contact us</a
+                    >
+                    or touch base at
+                    <a href="https://twitter.com/stemmechanics"
+                        >@stemmechanics</a
+                    >.
+                </td>
+            </tr>
+            <tr>
+                <td
+                    align="center"
+                    style="
+                        font-size: 80%;
+                        text-align: center;
+                        padding-top: 1rem;
+                        padding-bottom: 2rem;
+                    "
+                >
+                    Sent by STEMMechanics &middot;
+                    <a href="https://www.stemmechanics.com.au/"
+                        >Visit our Website</a
+                    >
+                    &middot;
+                    <a href="https://twitter.com/stemmechanics"
+                        >@stemmechanics</a
+                    ><br />PO Box 36, Edmonton, QLD 4869, Australia
+                </td>
+            </tr>
+        </table>
     </body>
 </html>
