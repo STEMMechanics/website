87d55b13f978c57e03ec3024553b65423f5427b6
James Collins <james.collins@outlook.com.au>
2023-01-26 15:08:07 +1000

    updated text

--- resources/js/views/Rules.vue
+++ resources/js/views/Rules.vue
@@ -1,25 +1,72 @@
 <template>
     <SMContainer class="rules">
         <h1>Rules</h1>
-        <h2>Discord</h2>
+        <p>
+            Oh gosh, no body likes rules but to ensure that we have a fun,
+            engaging and safe space for all across all our platform and
+            services, we have a set of rules that everyone must follow.
+        </p>
+        <p>
+            These are NOT a complete set of everything you can and can't do with
+            STEMMechanics staff and its helpers determining each offense on a
+            case-by-case basis.
+        </p>
+        <h2>Across all our services</h2>
+        <p>
+            Everywhere, whether physical or online, we don't tolerate the
+            following:
+        </p>
         <ul>
-            <li>No Spamming / Excessive Swearing</li>
-            <li>No Racism / Hate Speech</li>
-            <li>No NSFW content in any of our channels</li>
-            <li>No Doxxing</li>
-            <li>No Advertising</li>
-            <li>No sending malicious content</li>
-            <li>No Punishment evasion</li>
-            <li>No Excessive Trolling / Harrassment</li>
-            <li>No political or religious statements</li>
+            <li><strong>No Spamming / Excessive Swearing</strong></li>
+            <li>
+                <strong>No Racism / Hate Speech</strong> - Is not accepted under
+                any circumstance.
+            </li>
+            <li>
+                <strong>No NSFW content</strong> - Inappropriate, Adult,
+                Violence and/or Offensive content.
+            </li>
+            <li>
+                <strong>No Doxxing</strong> - Do not share your real name or
+                your address to others.
+            </li>
+            <li>
+                <strong>No Advertising</strong> - Referencing or pointing out a
+                service is ok.
+            </li>
+            <li><strong>No sending malicious content</strong></li>
+            <li><strong>No Punishment evasion</strong></li>
+            <li><strong>No Trolling / Harrassment</strong></li>
+            <li><strong>No political or religious statements</strong></li>
+        </ul>
+
+        <h2>Discord Server</h2>
+        <ul>
+            <li>
+                <strong>Nicknames / Profile Picture included</strong> - Your
+                nickname/profile picture must also follow the rules.
+            </li>
             <li>
                 Please follow Discord's
-                <a href="https://discord.com/terms">Terms of Service</a>
+                <a href="https://discord.com/terms">Terms of Service</a>.
             </li>
         </ul>
 
         <h2>Minecraft</h2>
         <ul>
+            <li>
+                <strong>No Begging</strong> - We don't allow begging. It's
+                annoying and no body likes it.
+            </li>
+            <li>
+                <strong>Use a Microsoft Account</strong> - The server is a
+                ONLINE server and will authenticate you against Microsoft
+                servers.
+            </li>
+            <li>
+                <strong>No Hacking Mods</strong> - Jesus, Camera, Dupes are all
+                included. Mini-maps are ok.
+            </li>
             <li>
                 <strong>No Griefing</strong> - Players are not permitted to
                 grief other players builds outside of the Survival game-mode.
