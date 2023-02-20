caffbb6c459344566033b33a7599bfde5b7903c4
James Collins <james.collins@outlook.com.au>
2023-02-20 19:45:22 +1000

    updated

--- resources/js/helpers/api.types.ts
+++ resources/js/helpers/api.types.ts
@@ -14,3 +14,33 @@ export interface ApiMediaItem {
 export interface ApiMedia {
     medium: ApiMediaItem;
 }
+
+export interface ApiPostItem {
+    title: string;
+    slug: string;
+    user_id: string;
+    content: string;
+    publish_at: string;
+    hero: string;
+}
+
+export interface ApiCollectionPost {
+    post: ApiPostItem;
+}
+
+export interface ApiCollectionPosts {
+    posts: Array<ApiPostItem>;
+}
+
+export interface ApiUser {
+    id: string;
+    username: string;
+}
+
+export interface ApiCollectionUser {
+    user: ApiUser;
+}
+
+export interface ApiCollectionUsers {
+    users: Array<ApiUser>;
+}
