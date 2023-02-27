a20551db7a0c70e9fc7653c7be1c63a4a28d7217
James Collins <james.collins@outlook.com.au>
2023-02-28 08:07:16 +1000

    fix template

--- resources/js/views/Contact.vue
+++ resources/js/views/Contact.vue
@@ -1,77 +1,81 @@
 <template>
-    <SMPage class="page-contact">
-        <SMRow break-large>
-            <SMColumn>
-                <h1 class="text-left">Contact Us</h1>
-                <h2>Questions & Support</h2>
-                <p>
-                    If you have a question or would like help with a project,
-                    you can send it our way using the form on this page or be
-                    emailing
-                    <a href="mailto:hello@stemmechanics.com.au"
-                        >hello@stemmechanics.com.au</a
-                    >.
-                </p>
-                <h2>Wanting a workshop?</h2>
-                <p>
-                    We provide both public and private workshops as well as run
-                    events on behalf of your organisation. If you would like to
-                    discuss a potential opportunity, send us an email at
-                    <a href="mailto:hello@stemmechanics.com.au"
-                        >hello@stemmechanics.com.au</a
-                    >.
-                </p>
-                <h2>Address</h2>
-                <p>
-                    We do not have a physical address as our workshops are
-                    delivered across Queensland. Visit the
-                    <router-link :to="{ name: 'workshop-list' }"
-                        >workshops</router-link
-                    >
-                    page for each specific location.
-                </p>
-                <p>Official mail can be sent to the following address:</p>
-                <div class="text-center">
-                    <p class="font-size-90">
-                        STEMMechanics<br />PO Box 36<br />Edmonton, QLD, 4869<br />Australia
+    <SMPage class="sm-page-contact">
+        <template #container>
+            <SMRow break-large>
+                <SMColumn>
+                    <h1 class="text-left">Contact Us</h1>
+                    <h2>Questions & Support</h2>
+                    <p>
+                        If you have a question or would like help with a
+                        project, you can send it our way using the form on this
+                        page or be emailing
+                        <a href="mailto:hello@stemmechanics.com.au"
+                            >hello@stemmechanics.com.au</a
+                        >.
                     </p>
-                    <p class="font-size-90">
-                        <strong>ABN: </strong>15 772 281 735
+                    <h2>Wanting a workshop?</h2>
+                    <p>
+                        We provide both public and private workshops as well as
+                        run events on behalf of your organisation. If you would
+                        like to discuss a potential opportunity, send us an
+                        email at
+                        <a href="mailto:hello@stemmechanics.com.au"
+                            >hello@stemmechanics.com.au</a
+                        >.
                     </p>
-                </div>
-            </SMColumn>
-            <SMColumn>
-                <div>
-                    <SMDialog narrow>
-                        <template v-if="!formSubmitted">
-                            <SMForm v-model="form" @submit="handleSubmit">
-                                <SMInput control="name" />
-                                <SMInput control="email" type="email" />
-                                <SMInput
-                                    control="content"
-                                    label="Message"
-                                    type="textarea" />
+                    <h2>Address</h2>
+                    <p>
+                        We do not have a physical address as our workshops are
+                        delivered across Queensland. Visit the
+                        <router-link :to="{ name: 'event-list' }"
+                            >workshops</router-link
+                        >
+                        page for each specific location.
+                    </p>
+                    <p>Official mail can be sent to the following address:</p>
+                    <div class="text-center">
+                        <p class="font-size-90">
+                            STEMMechanics<br />PO Box 36<br />Edmonton, QLD,
+                            4869<br />Australia
+                        </p>
+                        <p class="font-size-90">
+                            <strong>ABN: </strong>15 772 281 735
+                        </p>
+                    </div>
+                </SMColumn>
+                <SMColumn>
+                    <div>
+                        <SMDialog narrow>
+                            <template v-if="!formSubmitted">
+                                <SMForm v-model="form" @submit="handleSubmit">
+                                    <SMInput control="name" />
+                                    <SMInput control="email" type="email" />
+                                    <SMInput
+                                        control="content"
+                                        label="Message"
+                                        type="textarea" />
+                                    <SMButton
+                                        type="submit"
+                                        block
+                                        label="Send Message" />
+                                </SMForm>
+                            </template>
+                            <template v-else>
+                                <h1>Message Sent!</h1>
+                                <p class="text-center">
+                                    Your message as been sent.<br />We will
+                                    respond as soon as we can.
+                                </p>
                                 <SMButton
-                                    type="submit"
                                     block
-                                    label="Send Message" />
-                            </SMForm>
-                        </template>
-                        <template v-else>
-                            <h1>Message Sent!</h1>
-                            <p class="text-center">
-                                Your message as been sent.<br />We will respond
-                                as soon as we can.
-                            </p>
-                            <SMButton
-                                block
-                                :to="{ name: 'home' }"
-                                label="Home" />
-                        </template>
-                    </SMDialog>
-                </div>
-            </SMColumn>
-        </SMRow>
+                                    :to="{ name: 'home' }"
+                                    label="Home" />
+                            </template>
+                        </SMDialog>
+                    </div>
+                </SMColumn>
+            </SMRow>
+        </template>
     </SMPage>
 </template>
 
@@ -123,7 +127,7 @@ const handleSubmit = async () => {
 </script>
 
 <style lang="scss">
-.page-contact {
+.sm-page-contact {
     background-color: #f8f8f8;
 }
 </style>
