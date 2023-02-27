cc38a45fd0f5e1a9d902d63fe226903de2c8c0dc
James Collins <james.collins@outlook.com.au>
2023-02-28 08:49:54 +1000

    move and refactor scss

--- resources/js/components/SMModal.vue
+++ resources/js/components/SMModal.vue
@@ -15,3 +15,22 @@ onUnmounted(() => {
     document.getElementsByTagName("body")[0].style.overflow = "auto";
 });
 </script>
+
+<style lang="scss">
+.sm-modal {
+    position: fixed;
+    display: flex;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(0, 0, 0, 0.4);
+    backdrop-filter: blur(2px);
+    -webkit-backdrop-filter: blur(2px);
+    z-index: 1000;
+    padding: 1rem;
+}
+</style>
