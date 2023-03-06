b62a3b9d63305983a8e900cdc60f615328ef3c07
James Collins <james.collins@outlook.com.au>
2023-03-07 09:26:40 +1000

    updated java/bedrock

--- resources/js/views/Minecraft.vue
+++ resources/js/views/Minecraft.vue
@@ -4,8 +4,8 @@
             <h1>Connecting to our Minecraft Server</h1>
             <ol>
                 <li>
-                    Open up your Minecraft on your computer (Java) or tablet
-                    (Bedrock) and make sure you are using version 1.19.3
+                    Open up your Minecraft on your computer and make sure you
+                    are using version 1.19.3
                 </li>
                 <li>Click Multiplayer</li>
                 <li>Click Add Server</li>
@@ -18,6 +18,21 @@
                 <li>Click Done</li>
                 <li>Join the Server!</li>
             </ol>
+            <h2>Java or Bedrock</h2>
+            <p>
+                Regrettably, our support is exclusively for the Java edition of
+                Minecraft. The reason for this is that we rely on the features
+                unique to the Java edition, such as Resource Packs, Custom
+                Items, and Modelling, which are not available in Bedrock
+                (Tablet) edition.
+            </p>
+            <p>
+                It's worth noting that if you have the Bedrock version, you may
+                be eligible to acquire the Java version for free from
+                <a href="https://minecraft.net">Minecraft.net</a>. However,
+                please keep in mind that the Java version is solely compatible
+                with Windows or Mac operating systems.
+            </p>
             <h2>Goodbye Drustcraft</h2>
             <p>
                 STEMMechanics launched the Drustcraft server three years ago and
