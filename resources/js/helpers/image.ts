667972f05ee0583be35cf0e369a8111af9699eda
James Collins <james.collins@outlook.com.au>
2023-03-13 21:23:11 +1000

    added image size helper methods

--- resources/js/helpers/image.ts
+++ resources/js/helpers/image.ts
@@ -1,3 +1,5 @@
+import { ImportMetaExtras } from "../../../import-meta";
+
 type ImageLoadCallback = (url: string) => void;
 
 export const imageLoad = (
@@ -11,4 +13,34 @@ export const imageLoad = (
         callback(url);
     };
     tmp.src = url;
+};
+
+export const imageSize = (size: string, url: string) => {
+    const availableSizes = ["thumb", "medium", "large"];
+    if (availableSizes.includes(size)) {
+        if (
+            url.startsWith((import.meta as ImportMetaExtras).env.APP_URL) ===
+            true
+        ) {
+            return `${url}?size=${size}`;
+        }
+    }
+
+    return url;
+};
+
+export const imageThumb = (url: string) => {
+    return imageSize("thumb", url);
+};
+
+export const imageMedium = (url: string) => {
+    return imageSize("medium", url);
+};
+
+export const imageLarge = (url: string) => {
+    return imageSize("large", url);
+};
+
+export const imageFull = (url: string) => {
+    return imageSize("full", url);
 };
