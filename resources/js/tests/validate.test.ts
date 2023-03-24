d0d5cc68417b62327423163764511da3702e6612
James Collins <james.collins@outlook.com.au>
2023-03-24 12:02:45 +1000

    update tests to support new promise validation

--- resources/js/tests/validate.test.ts
+++ resources/js/tests/validate.test.ts
@@ -2,27 +2,27 @@ import { expect, describe, it } from "vitest";
 import { Email } from "../helpers/validate";
 
 describe("Email()", () => {
-    it("should return valid=false when an invalid email address is passed to the validate function", () => {
+    it("should return valid=false when an invalid email address is passed to the validate function", async () => {
         const v = Email();
-        const result = v.validate("invalid email");
+        const result = await v.validate("invalid email");
         expect(result.valid).toBe(false);
     });
 
-    it("should return valid=false when an invalid email address is passed to the validate function", () => {
+    it("should return valid=false when an invalid email address is passed to the validate function", async () => {
         const v = Email();
-        const result = v.validate("fake@outlook");
+        const result = await v.validate("fake@outlook");
         expect(result.valid).toBe(false);
     });
 
-    it("should return valid=true when an valid email address is passed to the validate function", () => {
+    it("should return valid=true when an valid email address is passed to the validate function", async () => {
         const v = Email();
-        const result = v.validate("fake@outlook.com");
+        const result = await v.validate("fake@outlook.com");
         expect(result.valid).toBe(true);
     });
 
-    it("should return valid=true when an valid email address is passed to the validate function", () => {
+    it("should return valid=true when an valid email address is passed to the validate function", async () => {
         const v = Email();
-        const result = v.validate("fake@outlook.com.au");
+        const result = await v.validate("fake@outlook.com.au");
         expect(result.valid).toBe(true);
     });
 });
