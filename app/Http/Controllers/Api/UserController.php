8b83b0c2125b65ebe476e457aa0f58068e0f083c
James Collins <james.collins@outlook.com.au>
2023-03-29 14:57:18 +1000

    added support for new responseAsResource format

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -71,7 +71,7 @@ class UserController extends ApiController
     {
         if (UserConductor::creatable() === true) {
             $user = User::create($request->all());
-            return $this->respondAsResource(UserConductor::model($request, $user), false, [], HttpResponseCodes::HTTP_CREATED);
+            return $this->respondAsResource(UserConductor::model($request, $user), false, [], null, HttpResponseCodes::HTTP_CREATED);
         } else {
             return $this->respondForbidden();
         }
