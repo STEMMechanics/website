c232042af5c75ea350f94921a547cf1715ebc86f
James Collins <james.collins@outlook.com.au>
2023-04-06 21:17:38 +1000

    use static function in Media modalk

--- app/Rules/UniqueFileName.php
+++ app/Rules/UniqueFileName.php
@@ -2,9 +2,8 @@
 
 namespace App\Rules;
 
+use App\Models\Media;
 use Illuminate\Contracts\Validation\Rule;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Storage;
 
 class UniqueFileName implements Rule
 {
@@ -27,16 +26,7 @@ class UniqueFileName implements Rule
      */
     public function passes($attribute, $value)
     {
-        // Get the list of storage disks from the filesystems configuration file
-        $disks = array_keys(Config::get('filesystems.disks'));
-
-        foreach ($disks as $disk) {
-            if (Storage::disk($disk)->exists($value) === true) {
-                return false;
-            }
-        }
-
-        return true;
+        return (Media::fileExists($value) === false);
     }
 
     /**
