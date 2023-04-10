ee46af08caad8bb28a5480c064d884327032a8b0
James Collins <james.collins@outlook.com.au>
2023-04-10 14:44:23 +1000

    update media table

---
+++ database/migrations/2023_04_05_222458_add_storage_description_variants_defaults_to_media_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            // Update null 'mime' values to empty strings
+            DB::table('media')->whereNull('mime')->update(['mime' => '']);
+
+            // Update null 'permission' values to empty strings
+            DB::table('media')->whereNull('permission')->update(['permission' => '']);
+
+            $table->renameColumn('mime', 'mime_type');
+
+            $table->bigInteger('size')->default(0)->change();
+            $table->string('mime_type')->default("")->nullable(false)->change();
+            $table->string('permission')->default("")->nullable(false)->change();
+
+            $table->string('storage')->default("cdn");
+            $table->string('description')->default("");
+            $table->string('status')->default("");
+            $table->string('dimensions')->default("");
+            $table->text('variants')->default("");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->bigInteger('size')->change();
+            $table->string('mime_type')->nullable(true)->change();
+            $table->string('permission')->nullable(true)->change();
+
+            $table->renameColumn('mime_type', 'mime');
+
+            $table->dropColumn('status');
+            $table->dropColumn('dimensions');
+            $table->dropColumn('variants');
+            $table->dropColumn('description');
+            $table->dropColumn('storage');
+        });
+    }
+};
