171cfa7aab8da7a148c2373ed968aeb1da0a85dd
James Collins <james.collins@outlook.com.au>
2023-05-06 18:16:53 +1000

    added table

---
+++ database/migrations/2023_05_06_080418_create_shortlinks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shortlinks', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 4)->unique();
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shortlinks');
+    }
+};
