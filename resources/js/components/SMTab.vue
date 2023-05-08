ffcf823a7f6943b3dbe6865174e0bea376dbb428
James Collins <james.collins@outlook.com.au>
2023-05-08 15:16:50 +1000

    reduced top margin

--- resources/js/components/SMTab.vue
+++ resources/js/components/SMTab.vue
@@ -19,7 +19,7 @@ const selectedLabel = inject("selectedLabel");
 
 <style lang="scss">
 .tab-content {
-    padding: 32px 32px 16px 32px;
+    padding: 16px 32px 16px 32px;
     background-color: var(--tab-color);
     border-width: 0 1px 1px 1px;
     border-style: solid;
