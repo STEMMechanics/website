78f23db801df77873e00cdde2b58419c662ceb35
James Collins <james.collins@outlook.com.au>
2023-05-21 07:50:29 +1000

    updated maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  img { margin: 0 auto; }
+  h1 { text-align: center; font-size: 175%; }
+  a, a:visited { color: #35a5f1; text-decoration-thickness: 0.1em; text-decoration-color: #67bbf4; }
+  a:hover { filter: brightness(115%); }
+  p { line-height: 1.25em; }
+  .d-light { display: block; }
+  .d-dark { display: none; }
+
+  @media (prefers-color-scheme: dark) {
+    body { background-color: #333; color: #ccc; }
+    .d-light { display: none; }
+    .d-dark { display: block; }
+  }
+</style>
+
+<article>
+    <img class="d-light" src="/assets/logo.webp" width="312" height="48" />
+    <img class="d-dark" src="/assets/logo-dark.webp" width="312" height="48" />
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always contact us by <a href="mailto:hello@stemmechanics.com.au">email</a> or over on <a href="https://discord.gg/yNzk4x7mpD">Discord</a>, otherwise we&rsquo;ll be back online shortly!</p>
+        <p>&mdash; The STEMMechanics Team</p>
+    </div>
+</article>
