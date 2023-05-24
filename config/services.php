b68d97455b3fb2b5a27e891f1250f1f6c33d739f
James Collins <james.collins@outlook.com.au>
2023-05-25 07:51:21 +1000

    remove gunk

--- config/services.php
+++ config/services.php
@@ -30,14 +30,4 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-<<<<<<< HEAD
-
-    'google_recaptcha' => [
-        'url' => 'https://www.google.com/recaptcha/api/siteverify',
-        'site_key' => env('GOOGLE_RECAPTCHA_SITE_KEY'),
-        'secret_key' => env('GOOGLE_RECAPTCHA_SECRET_SITE_KEY'),
-    ],
-
-=======
->>>>>>> 086cf9e (removed obsolete recaptcha)
 ];
