d4e832b526cce51b9339278849ecb5bff0ecf28a
James Collins <james.collins@outlook.com.au>
2023-06-22 17:28:16 +1000

    show loader

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,6 +1,7 @@
 <template>
     <SMNavbar />
     <main class="flex-1">
+        <SMLoading v-if="!appLoaded" class="h-95" />
         <router-view v-slot="{ Component }">
             <component :is="Component" />
         </router-view>
@@ -11,8 +12,16 @@
 </template>
 
 <script setup lang="ts">
+const appLoaded = ref(false);
+
 import SMNavbar from "../components/SMNavbar.vue";
 import SMPageFooter from "../components/SMPageFooter.vue";
 import SMToastList from "../components/SMToastList.vue";
 import SMDialogList from "../components/SMDialog";
+import SMLoading from "../components/SMLoading.vue";
+import { onMounted, ref } from "vue";
+
+onMounted(() => {
+    appLoaded.value = true;
+});
 </script>
