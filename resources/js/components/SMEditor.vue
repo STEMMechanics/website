64ef8e9acdaf682f99b64041ad07ae1e054b581e
James Collins <james.collins@outlook.com.au>
2023-07-17 11:24:57 +1000

    removed gallery item

--- resources/js/components/SMEditor.vue
+++ resources/js/components/SMEditor.vue
@@ -369,27 +369,6 @@
                             fill="currentColor" />
                     </svg>
                 </button>
-                <button
-                    @click.prevent="setLink()"
-                    title="gallery"
-                    :class="[
-                        'flex',
-                        'flex-items-center',
-                        'p-1',
-                        'hover-bg-gray-3',
-                        editor.isActive('gallery')
-                            ? ['bg-sky-6', 'text-white']
-                            : ['bg-white', 'text-gray-6'],
-                    ]">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-4 w-5"
-                        viewBox="0 0 24 24">
-                        <path
-                            d="M21,17H7V3H21M21,1H7A2,2 0 0,0 5,3V17A2,2 0 0,0 7,19H21A2,2 0 0,0 23,17V3A2,2 0 0,0 21,1M3,5H1V21A2,2 0 0,0 3,23H19V21H3M15.96,10.29L13.21,13.83L11.25,11.47L8.5,15H19.5L15.96,10.29Z"
-                            fill="currentColor" />
-                    </svg>
-                </button>
             </div>
             <div class="flex p-1 border-r">
                 <button
