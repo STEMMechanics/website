9996404e29dc7fb18b9bc2a687535bfad3032f94
James Collins <james.collins@outlook.com.au>
2023-07-19 15:00:07 +1000

    missing trait

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,18 +2,19 @@
 
 namespace App\Models;
 
+use App\Traits\HasAttachments;
 use App\Traits\HasGallery;
 use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Article extends Model
 {
     use HasFactory;
     use Uuids;
     use HasGallery;
+    use HasAttachments;
 
     /**
      * The attributes that are mass assignable.
