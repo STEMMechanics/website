351259909c9c457c4092fc3a033dd99d8c5defce
James Collins <james.collins@outlook.com.au>
2023-08-29 15:02:33 +1000

    fix rule validation

--- app/Http/Requests/MediaRequest.php
+++ app/Http/Requests/MediaRequest.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
+
 class MediaRequest extends BaseRequest
 {
     public function postRules(): array
     {
         return [
-            'id' => 'required_with:chunk|string',
+            'id' => [
+                Rule::requiredIf(function () {
+                    return request()->has('chunk') && request('chunk') != 1;
+                }),
+                'string',
+            ],
             'chunk' => 'required_with:chunk_count|integer|min:1|max:99',
             'chunk_count' => 'required_with:chunk|integer|min:1',
         ];
