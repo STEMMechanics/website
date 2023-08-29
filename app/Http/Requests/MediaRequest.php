eada83aaf8febb88b07641aca1ed4926b12c4814
James Collins <james.collins@outlook.com.au>
2023-08-29 14:34:51 +1000

    added chunking rules

--- app/Http/Requests/MediaRequest.php
+++ app/Http/Requests/MediaRequest.php
@@ -4,5 +4,12 @@ namespace App\Http\Requests;
 
 class MediaRequest extends BaseRequest
 {
-    /* empty */
+    public function postRules(): array
+    {
+        return [
+            'id' => 'required_with:chunk|string',
+            'chunk' => 'required_with:chunk_count|integer|min:1|max:99',
+            'chunk_count' => 'required_with:chunk|integer|min:1',
+        ];
+    }
 }
