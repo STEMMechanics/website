f6df06878753a6d1d092091d41c32d88b4a84d91
James Collins <james.collins@outlook.com.au>
2023-09-29 11:01:37 +1000

    fix bad local storage url

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,7 +33,7 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . "/storage",
+            'url' => env('APP_URL') . "/storage/{name}",
             'public' => true,
             'throw' => false,
         ],
