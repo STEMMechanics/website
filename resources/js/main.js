451439681832dd3763f1273b0fafd39b03bec1d9
James Collins <james.collins@outlook.com.au>
2024-01-22 12:12:15 +1000

    change

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,39 @@
-import Router from "@/router";
-import { createPinia } from "pinia";
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
-import { createApp } from "vue";
-import App from "./views/App.vue";
-import "uno.css";
-import "../css/app.scss";
+import { createInertiaApp } from "@inertiajs/vue3";
+import Toast, { POSITION } from "vue-toastification";
+import "vue-toastification/dist/index.css";
+import { createApp, h } from "vue/dist/vue.esm-bundler.js";
+import "./bootstrap";
 
-const pinia = createPinia();
-pinia.use(piniaPluginPersistedstate);
-
-createApp(App).use(pinia).use(Router).mount("#app");
+createInertiaApp({
+    title: (title) => `${title} - STEMMechanics`,
+    progress: {
+        delay: 250,
+        color: "#22C55E",
+        includeCSS: true,
+        showSpinner: false,
+    },
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        return pages[`./Pages/${name}.vue`];
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(Toast, {
+                position: POSITION.TOP_CENTER,
+                toastClassName: "stemmechanics-toast",
+                transition: "toast",
+                hideProgressBar: true,
+            })
+            .mixin({ methods: { route } })
+            .directive("inject-svg", {
+                mounted(el) {
+                    window.SVGInject(el);
+                },
+                updated(el) {
+                    window.SVGInject(el);
+                },
+            })
+            .mount(el);
+    },
+});
