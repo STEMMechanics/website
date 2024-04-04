5fbca80a3c681b31b5b31ffa1f9690cea374b637
James Collins <james.collins@outlook.com.au>
2024-04-04 20:59:59 +1000

    disable Intervention\Image\ImageServiceProvider::class

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Intervention\Image\ImageServiceProvider::class,
+        // Intervention\Image\ImageServiceProvider::class,
         OwenIt\Auditing\AuditingServiceProvider::class,
 
         /*
