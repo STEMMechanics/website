28b2ffa4a37c29f0efa5f33d59fcdb55ad5e1dcb
James Collins <james.collins@outlook.com.au>
2024-04-23 21:38:45 +1000

    only need lg images

--- resources/views/components/ui/image-hero.blade.php
+++ resources/views/components/ui/image-hero.blade.php
@@ -2,5 +2,5 @@
 
 <div class="{{twMerge('relative w-full h-96 flex items-center justify-center bg-cover bg-center rounded-3xl overflow-hidden', $attributes->get('class'))}}">
     <div class="blur bg-cover bg-center absolute top-0 left-0 w-full h-full opacity-50" style="background-image: url('{{ $image }}')"></div>
-    <img src="{{ $image }}" class="h-full z-0" />
+    <img src="{{ $image }}?lg" class="h-full z-0" />
 </div>
