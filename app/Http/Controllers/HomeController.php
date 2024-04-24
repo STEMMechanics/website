f46dbd887be71e0f42edec58c240eea51190c0a9
James Collins <james.collins@outlook.com.au>
2024-04-24 17:56:40 +1000

    fix workshop order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
     public function index()
     {
         $posts = Post::query()->orderBy('created_at', 'desc')->limit(4)->get();
-        $workshops = Workshop::query()->where('starts_at', '>', now())->where('status', '!=', 'private')->orderBy('created_at', 'asc')->limit(4)->get();
+        $workshops = Workshop::query()->where('starts_at', '>', now())->where('status', '!=', 'private')->orderBy('starts_at', 'asc')->limit(4)->get();
 
         return view('home', [
             'posts' => $posts,
