47a2afff863a79aa20d30aed456305dd07c0ad57
James Collins <james.collins@outlook.com.au>
2024-04-28 18:24:56 +1000

    added filetype

--- resources/views/admin/media/index.blade.php
+++ resources/views/admin/media/index.blade.php
@@ -17,6 +17,7 @@
             <x-ui.table>
                 <x-slot:header>
                     <th>Title</th>
+                    <th class="hidden md:table-cell">Type</th>
                     <th class="hidden md:table-cell">Size</th>
                     <th class="hidden md:table-cell">Uploaded</th>
                     <th>Action</th>
@@ -28,9 +29,11 @@
                                 <img src="{{ $medium->thumbnail }}" class="max-h-12 max-w-12 -ml-2 -my-3 mr-3 inline rounded" alt="{{ $medium->title }}" />
                                 <div>
                                     <div class="whitespace-normal">{{ $medium->title }}{!! $medium->password !== null ? '<i class="fa-solid fa-lock text-xs text-gray-400 ml-0.5 -translate-y-1.5 scale-75"></i>': '' !!}</div>
+                                    <div class="md:hidden text-xs text-gray-500">{{ $medium->file_type }}</div>
                                     <div class="md:hidden text-xs text-gray-500">{{ \Carbon\Carbon::parse($medium->created_at)->format('j/m/Y') }} - {{ \App\Helpers::bytesToString($medium->size) }}</div>
                                 </div>
                             </td>
+                            <td class="hidden md:table-cell">{{ $medium->file_type }}</td>
                             <td class="hidden md:table-cell">{{ \App\Helpers::bytesToString($medium->size) }}</td>
                             <td class="hidden md:table-cell">{{ \Carbon\Carbon::parse($medium->created_at)->format('M j Y, g:i a') }}</td>
                             <td>
