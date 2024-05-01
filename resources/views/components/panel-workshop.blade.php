30e308466bdf1456e7e9ac6cfd80174143abc668
James Collins <james.collins@outlook.com.au>
2024-05-01 21:06:40 +1000

    update title size based on length

--- resources/views/components/panel-workshop.blade.php
+++ resources/views/components/panel-workshop.blade.php
@@ -18,7 +18,7 @@
     <div class="border border-white absolute flex items-center justify-center top-5 -right-9 bg-gray-500 w-36 text-sm text-white font-bold uppercase py-1 rotate-45 h-8 sm-banner-{{ strtolower($statusClass) }}">{{ $statusTitle }}</div>
     <img src="{{ $workshop->hero?->url }}?md" alt="{{ $workshop->title }}" class="w-full h-64 object-cover object-center">
     <div class="flex-grow p-4 flex flex-col">
-        <h2 class="flex-grow text-xl font-bold mb-2">{{ $workshop->title }}</h2>
+        <h2 class="flex-grow {{ strlen($workshop->title) > 25 ? 'text-lg' : 'text-xl' }} font-bold mb-2">{{ $workshop->title }}</h2>
         <div class="text-gray-600 text-sm mb-1 flex gap-2">
             <div class="w-6 flex items-center justify-center">
                 <i class="fa-regular fa-calendar"></i>
