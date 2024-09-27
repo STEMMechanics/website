49eb388041c5518ddd5324532402324e2c4f034a
James Collins <james.collins@outlook.com.au>
2024-09-27 11:17:49 +1000

    updated mast to support tabs

--- resources/views/components/mast.blade.php
+++ resources/views/components/mast.blade.php
@@ -1,6 +1,13 @@
 <x-container class="bg-primary-color-light text-white py-10">
-    <h1 class="font-bold text-4xl">{{ $slot }}</h1>
+    <h1 class="font-bold text-4xl">{{ $title ?? $slot }}</h1>
     @if(isset($backRoute) && isset($backTitle))
         <a href="{{ route($backRoute) }}" class="text-lg hover:text-gray-300"><i class="fa-solid fa-angle-left mr-3"></i>{{ $backTitle }}</a>
     @endif
+    @isset($tabs)
+        <div class="mt-4 -mb-10 flex justify-end">
+            @foreach($tabs as $tab)
+                <a href="{{ $tab['route'] }}" class="rounded-t-md px-4 py-2 {{ (request()->routeIs($tab['route']) ? 'bg-gray-100 text-primary-color-dark' : 'text-white') }} hover:bg-primary-color-dark transition-colors">{{ $tab['title'] }}</a>
+            @endforeach
+        </div>
+    @endisset
 </x-container>
