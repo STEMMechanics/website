6bc2b888a4f93ad6ad5fb1d813f2fb98f67ae88b
James Collins <james.collins@outlook.com.au>
2024-09-28 09:18:43 +1000

    change timer

--- routes/console.php
+++ routes/console.php
@@ -32,11 +32,7 @@ Artisan::command('email:send', function() {
     foreach ($subscribers as $subscriber) {
         dispatch(new SendEmail($subscriber->email, new UpcomingWorkshops($subscriber->email, $subject)))->onQueue('mail');
     }
-})->purpose('Send newsletter to confirmed subscribers')->weeklyOn(3, function() {
-    $hour = random_int(16, 18);
-    $minute = random_int(0, 59);
-    return sprintf('%02d:%02d', $hour, $minute);
-});
+})->purpose('Send newsletter to confirmed subscribers')->weeklyOn(3, '16:00');
 
 Artisan::command('cleanup', function() {
 
