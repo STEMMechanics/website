954b83ebbad9f37438e5541eada7de1a045d18ec
James Collins <james.collins@outlook.com.au>
2026-02-03 08:36:00 +0000

    Update renovate-config.json

--- renovate-config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "endpoint": "https://git.stemmechanics.com.au/api/v1",
-    "gitAuthor": "Renovate Bot <renovate-bot@stemmechanics.com.au>",
-    "platform": "gitea",
-    "onboardingConfigFileName": "renovate.json",
-    "autodiscover": true,
-    "optimizeForDisabled": true,
-};
