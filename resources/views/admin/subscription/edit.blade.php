562c6614987e95110eeedb282190e218bfe2c198
James Collins <james.collins@outlook.com.au>
2026-02-14 18:46:02 +1000

    added confirm date field

--- resources/views/admin/subscription/edit.blade.php
+++ resources/views/admin/subscription/edit.blade.php
@@ -4,16 +4,19 @@
     <x-container class="mt-4">
         <form method="POST" action="{{ route('admin.subscription.' . ( isset($subscription) ? 'update' : 'store'), $subscription ?? []) }}">
             @isset($subscription)
-                @method('PUT')
+            @method('PUT')
             @endisset
             @csrf
 
             <div class="mb-4">
                 <x-ui.input label="Email" name="email" type="email" value="{{ $subscription->email ?? '' }}" />
             </div>
+            <div class="mb-4">
+                <x-ui.input label="Registered On" name="confirmed" value="{{ $subscription->confirmed ? \Carbon\Carbon::parse($subscription->confirmed)->format('M j Y, g:i a') : '-' }}" disabled />
+            </div>
             <div class="flex justify-end mt-8 gap-4">
                 @isset($subscription)
-                    <x-ui.button type="button" color="danger" x-data x-on:click.prevent="SM.confirmDelete('{{ csrf_token() }}', 'Delete subscription?', 'Are you sure you want to delete this subscription? This action cannot be undone', '{{ route('admin.subscription.destroy', $subscription) }}')">Delete</x-ui.button>
+                <x-ui.button type="button" color="danger" x-data x-on:click.prevent="SM.confirmDelete('{{ csrf_token() }}', 'Delete subscription?', 'Are you sure you want to delete this subscription? This action cannot be undone', '{{ route('admin.subscription.destroy', $subscription) }}')">Delete</x-ui.button>
                 @endisset
                 <x-ui.button type="submit">Save</x-ui.button>
             </div>
