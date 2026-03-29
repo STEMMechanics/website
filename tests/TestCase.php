4d770e6eff7243a150d911184830e5f881730800
James Collins <james.collins@outlook.com.au>
2026-03-29 19:14:50 +1000

    setup passport test keys

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,80 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Laravel\Passport\Passport;
+use RuntimeException;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    private static bool $passportKeysEnsured = false;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->ensurePassportKeysExist();
+    }
+
+    private function ensurePassportKeysExist(): void
+    {
+        if (self::$passportKeysEnsured) {
+            return;
+        }
+
+        Passport::loadKeysFrom(storage_path());
+
+        $privateKeyPath = storage_path('oauth-private.key');
+        $publicKeyPath = storage_path('oauth-public.key');
+
+        if ($this->passportKeysAreValid($privateKeyPath, $publicKeyPath)) {
+            self::$passportKeysEnsured = true;
+
+            return;
+        }
+
+        $config = [
+            'private_key_bits' => 2048,
+            'private_key_type' => OPENSSL_KEYTYPE_RSA,
+        ];
+
+        $keyResource = openssl_pkey_new($config);
+        if ($keyResource === false) {
+            throw new RuntimeException('Unable to generate Passport test keys.');
+        }
+
+        if (! openssl_pkey_export($keyResource, $privateKey)) {
+            throw new RuntimeException('Unable to export Passport private key.');
+        }
+
+        $details = openssl_pkey_get_details($keyResource);
+        $publicKey = $details['key'] ?? null;
+        if (! is_string($publicKey) || $publicKey === '') {
+            throw new RuntimeException('Unable to export Passport public key.');
+        }
+
+        file_put_contents($privateKeyPath, $privateKey);
+        file_put_contents($publicKeyPath, $publicKey);
+
+        @chmod($privateKeyPath, 0600);
+        @chmod($publicKeyPath, 0644);
+
+        self::$passportKeysEnsured = true;
+    }
+
+    private function passportKeysAreValid(string $privateKeyPath, string $publicKeyPath): bool
+    {
+        if (! is_file($privateKeyPath) || ! is_file($publicKeyPath)) {
+            return false;
+        }
+
+        $privateKey = file_get_contents($privateKeyPath);
+        $publicKey = file_get_contents($publicKeyPath);
+
+        if (! is_string($privateKey) || ! is_string($publicKey)) {
+            return false;
+        }
+
+        return openssl_pkey_get_private($privateKey) !== false
+            && openssl_pkey_get_public($publicKey) !== false;
+    }
 }
