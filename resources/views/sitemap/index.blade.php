d828d72774c0229f794280b2b85e4d6628bfcc0b
James Collins <james.collins@outlook.com.au>
2026-09-17 13:43:13 +1000

    Fix blade rendering xml tags

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,4 +1,4 @@
-{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
+@php echo '<?xml version="1.0" encoding="UTF-8"?>'; @endphp
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @for($page = 1; $page <= $pageCount; $page++)
     <sitemap><loc>{{ route('sitemap.xml', ['page' => $page]) }}</loc></sitemap>
