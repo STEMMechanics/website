0b419ed172f8dde954e4b6dcb1cd067bfc05c99c
James Collins <james.collins@outlook.com.au>
2026-09-22 08:35:56 +1000

    update layout

--- resources/views/workshop/tickets/review.blade.php
+++ resources/views/workshop/tickets/review.blade.php
@@ -13,7 +13,7 @@
                     <h3 class="mb-3 text-lg font-semibold">Who’s coming?</h3>
                     <template x-for="(person, index) in participants" :key="index">
                         <div class="mb-3 rounded-lg border border-gray-200 p-3">
-                            <div class="flex items-start justify-between gap-2"><h4 class="font-semibold" x-text="'Participant ' + (index + 1)"></h4><x-ui.row-action icon="fa-trash" label="Remove participant" x-show="participants.length > 1" x-on:click="participants.splice(index, 1)" /></div>
+                            <div class="flex items-start justify-between gap-2 h-11"><h4 class="font-semibold" x-text="'Participant ' + (index + 1)"></h4><x-ui.row-action icon="fa-trash" label="Remove participant" x-show="participants.length > 1" x-on:click="participants.splice(index, 1)" /></div>
                             <label class="mb-3 block text-sm font-medium">First name<x-ui.input-control x-bind:name="`participants[${index}][firstname]`" x-model="person.firstname" autocomplete="off" required /></label>
                             <label class="mb-3 block text-sm font-medium">Surname<x-ui.input-control x-bind:name="`participants[${index}][surname]`" x-model="person.surname" autocomplete="off" required /></label>
                         </div>
